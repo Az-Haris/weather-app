5b4a6a04db13d045915e88349a34a387acd54d8f
MD Alimuzzaman Haris <mdalimuzzaman437@gmail.com>
2024-10-22 13:53:58 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// src/components/Header.jsx
+import React from 'react';
+import '../styles/Header.css'; // Import CSS for the header
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header className="app-header">
+            <a href="https://mteverse.vercel.app/"><img className="logo" src="/ScreenShots/logo.png" alt="MTEVERSE"/></a>
+            <h1>MTEverse Weather Reporter</h1>
+        </header>
+    );
+}
+
+export default Header;
